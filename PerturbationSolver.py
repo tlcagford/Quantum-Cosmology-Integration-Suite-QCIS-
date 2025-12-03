d195b1922cd9d24d2c6d0ed017a3f4631dafaaac
Tony E Ford <tlcagford@gmail.com>
2025-12-02 18:22:11 -0700

    Create PerturbationSolver.py

---
+++ PerturbationSolver.py
@@ -0,0 +1,14 @@
+class PerturbationSolver:
+    """Solve perturbation equations with quantum corrections"""
+    def __init__(self, background_solution):
+        self.background = background_solution
+        
+    def solve_metric_perturbations(self, k_values):
+        """Solve for Φ, Ψ perturbations"""
+        # Implement Mukhanov-Sasaki equation with quantum corrections
+        pass
+        
+    def compute_cmb_spectra(self):
+        """Compute CMB TT, TE, EE spectra"""
+        # Use CAMB-like calculation with modified sources
+        pass
