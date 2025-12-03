4a37c1a503d6dcc3c695b01e2d3302ca7ac387ff
Tony E Ford <tlcagford@gmail.com>
2025-12-02 18:25:37 -0700

    Create Adiabatic regularization.py

---
+++ Adiabatic regularization.py
@@ -0,0 +1,9 @@
+# Missing: Adiabatic regularization
+def adiabatic_subtraction(self, mode_functions, order=4):
+    # WKB expansion of mode functions
+    # Subtract divergent terms up to given order
+    # This is mathematically complex
+    pass
+
+# Missing: Point-splitting regularization
+# Missing: Dimensional regularization in curved space
